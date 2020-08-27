0a3fb564fa5d51c545cfa82e114a8734e23e151a
roy <1216172441@qq.com>
2020-08-28 03:15:55 +0800

    [homework][roy] test: pass should_return_A_when_rating_given_history_length_5_and_voyage_with_china_and_length_2

--- test/rankTest.js
+++ test/rankTest.js
@@ -36,3 +36,16 @@ test('should_return_A_when_rating_given_voyage_with_china_and_length_2', t => {
   const result = rating(voyage, history);
   t.is(result, 'A');
 })
+
+test('should_return_A_when_rating_given_history_length_5_and_voyage_with_china_and_length_2', t => {
+  const voyage = { zone: 'china', length: 2 };
+  const history = [
+    { zone: 'east-indies', profit: 5 },
+    { zone: 'west-indies', profit: 15 },
+    { zone: 'china', profit: -2 },
+    { zone: 'west-africa', profit: 7 },
+    { zone: 'east-africa', profit: 7 }
+  ];
+  const result = rating(voyage, history);
+  t.is(result, 'A');
+})

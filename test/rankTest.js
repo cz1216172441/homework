5fe1dce734da126178c2ce886e3ca89c5672d302
roy <1216172441@qq.com>
2020-08-28 03:05:44 +0800

    [homework][roy] test: pass should_return_A_when_rating_given_voyage_with_west_indies_and_length_10

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,10 +1,14 @@
-const rankTest = require('ava');
+const test = require('ava');
+const { rating } = require('../src/rank');
 
-rankTest('foo', t => {
-  t.pass();
-});
-
-rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
-});
+test('should_return_A_when_rating_given_voyage_with_west_indies_and_length_10', t => {
+  const voyage = { zone: 'west-indies', length: 10 };
+  const history = [
+    { zone: 'east-indies', profit: 5 },
+    { zone: 'west-indies', profit: 15 },
+    { zone: 'china', profit: -2 },
+    { zone: 'west-africa', profit: 7 }
+  ];
+  const result = rating(voyage, history);
+  t.is(result, 'A');
+})

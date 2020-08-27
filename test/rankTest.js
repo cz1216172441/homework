9416e0b199b0649c26da8e51fc8e7b4bc59b23c2
roy <1216172441@qq.com>
2020-08-28 03:11:41 +0800

    [homework][roy] test: pass should_return_A_when_rating_given_voyage_with_china_and_length_2

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,7 +1,7 @@
 const test = require('ava');
 const { rating } = require('../src/rank');
 
-test('should_return_A_when_rating_given_voyage_with_west_indies_and_length_10', t => {
+test('should_return_B_when_rating_given_voyage_with_west_indies_and_length_10', t => {
   const voyage = { zone: 'west-indies', length: 10 };
   const history = [
     { zone: 'east-indies', profit: 5 },
@@ -10,11 +10,23 @@ test('should_return_A_when_rating_given_voyage_with_west_indies_and_length_10', 
     { zone: 'west-africa', profit: 7 }
   ];
   const result = rating(voyage, history);
-  t.is(result, 'A');
+  t.is(result, 'B');
 })
 
-test('should_return_A_when_rating_given_voyage_with_west_indies_and_length_2', t => {
+test('should_return_B_when_rating_given_voyage_with_west_indies_and_length_2', t => {
   const voyage = { zone: 'west-indies', length: 2 };
+  const history = [
+    { zone: 'east-indies', profit: 5 },
+    { zone: 'west-indies', profit: 15 },
+    { zone: 'china', profit: -2 },
+    { zone: 'west-africa', profit: 7 }
+  ];
+  const result = rating(voyage, history);
+  t.is(result, 'B');
+})
+
+test('should_return_A_when_rating_given_voyage_with_china_and_length_2', t => {
+  const voyage = { zone: 'china', length: 2 };
   const history = [
     { zone: 'east-indies', profit: 5 },
     { zone: 'west-indies', profit: 15 },

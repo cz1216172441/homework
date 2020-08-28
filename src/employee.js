19bcd475d7adb82f12a69aa6b5a95c42f395e2ea
roy <1216172441@qq.com>
2020-08-29 02:47:24 +0800

    [homework][roy] refactor: create 3 different type employee class and add a createEmployee method

--- src/employee.js
+++ src/employee.js
@@ -28,4 +28,29 @@ class Employee {
   }
 }
 
+class Engineer extends Employee {
+  get type() {
+    return 'engineer';
+  }
+}
+class Manager extends Employee {
+  get type() {
+    return 'manager';
+  }
+}
+class Salesman extends Employee {
+  get type() {
+    return 'salesman';
+  }
+}
+
+function createEmployee(name, type) {
+  switch (type) {
+    case 'engineer': return new Engineer(name, type);
+    case 'manager': return new Manager(name, type);
+    case 'salesman': return new Salesman(name, type);
+    default: throw new Error(`Employee cannot be of type ${type}`);
+  }
+}
+
 export default Employee;

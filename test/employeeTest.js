0d10f77ee5a208176d179995dbd4ee85b9d827d7
roy <1216172441@qq.com>
2020-08-29 02:24:47 +0800

    [homework][roy] test: pass should_return_employee_when_new_employee_given_type_manager

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -6,3 +6,9 @@ test('should_return_employee_when_new_employee_given_type_engineer', t => {
   const actualResult = employee.toString();
   t.is('roy (engineer)', actualResult);
 })
+
+test('should_return_employee_when_new_employee_given_type_manager', t => {
+  const employee = new Employee('roy', 'manager');
+  const actualResult = employee.toString();
+  t.is('roy (manager)', actualResult);
+})

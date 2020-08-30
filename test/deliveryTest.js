a396dbc3742b55e1d1660f211a99b787c67450b3
roy <1216172441@qq.com>
2020-08-30 16:39:23 +0800

    [homework][roy] test: pass should_add_5_days_when_delivery_date_given_not_rush_and_an_order_with_delivery_state_ME

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -100,3 +100,15 @@ test('should_add_4_days_when_delivery_date_given_not_rush_and_an_order_with_deli
   const actualResult = deliveryDate(anOrder, isRush);
   t.is(new Date('2020-09-03').getTime(), actualResult);
 })
+
+test('should_add_5_days_when_delivery_date_given_not_rush_and_an_order_with_delivery_state_ME', t => {
+  let anOrder = {
+    deliveryState: 'ME',
+    placedOn: {
+      plusDays: plusDays
+    }
+  }
+  const isRush = false;
+  const actualResult = deliveryDate(anOrder, isRush);
+  t.is(new Date('2020-09-04').getTime(), actualResult);
+})
